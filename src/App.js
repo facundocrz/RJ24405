68fb2224f4fc55ce19831a84080a1bab21a5b40b
facundo cruz <facundocruz.m@gmail.com>
2022-01-12 22:30:09 -0300

    carrito navbar y ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import ItemListCointainer from './components/ItemListCointainer';
 
 function App() {
   return (
     <>
     <NavBar />
+    <ItemListCointainer greeting = "Bienvenido" />
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,35 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Container, Button} from '@mui/material'
+import {Box, AppBar, Toolbar, Typography, Container, Button} from '@mui/material'
+import {ShoppingCart} from '@mui/icons-material'
 
 export default function NavBar() {
     return (
         <>
-            <AppBar position="static">
-                <Container maxWidth="lg">
-                    <Toolbar>
-                        <Typography variant="h6">
-                            TIENDA
-                        </Typography>
-                          <div> 
-                              <Button variant="text" color="inherit">
-                                productos
-                              </Button>
-                              <Button variant="text" color="inherit">
-                                ayuda
-                              </Button>
-                              <Button variant="text" color="inherit">
-                                contacto
-                              </Button>
-                          </div>
-                    </Toolbar>
-                </Container>
-            </AppBar>
+            <Box sx={{flexGrow: 1}}>
+                <AppBar position="static">
+                    <Container maxWidth="lg">
+                        <Toolbar>
+                            <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                                TIENDA
+                            </Typography>
+                            <div> 
+                                <Button variant="text" color="inherit">
+                                    productos
+                                </Button>
+                                <Button variant="text" color="inherit">
+                                    ayuda
+                                </Button>
+                                <Button variant="text" color="inherit">
+                                    contacto
+                                </Button>
+                            </div>
+                            <Button variant="icon" color="inherit">
+                                <ShoppingCart/>
+                            </Button>
+                        </Toolbar>
+                    </Container>
+                </AppBar>
+            </Box>
         </>
     )
 }
